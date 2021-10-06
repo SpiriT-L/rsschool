9f4a3520b997e6f02840aee5884ca8387fa51a0f
SpiriT-L <m.leon-id@yandex.by>
2021-10-06 14:50:22 +0300

    feat: environment configuration

---
+++ form/src/index.ts
@@ -0,0 +1,11 @@
+// import './index.js';
+import './scss/style.scss';
+// import './index.html';
+// import '../js/index.js';
+// import './assets/images/[name].png';
+// import {test} from '../test';
+
+
+
+// console.log('Hellow World!')
+// test();
