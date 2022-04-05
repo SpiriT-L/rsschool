9c6f5b469eca9bbae4710c2a2407f2a20b539e08
LeonID <lmatskevich79@gmail.com>
2022-04-05 15:08:18 +0300

    feat: add dev-server 43_34

--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -1,15 +1,30 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
-const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, 'dist'),
+    // contentBase: path.join(__dirname, 'dist'),
+    open: {
+      app: {
+        name: 'chrome',
+        arguments: ['--incognito' /* , '--new-window' */],
+      },
+    },
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './js/main.js',
@@ -23,24 +38,42 @@ module.exports = {
       filename: 'index.html',
       inject: 'body',
       minify: {
-        collapseWhitespace: isProd
-      }
+        collapseWhitespace: isProd,
+      },
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: `./css/${filename('css')}`,
-    })
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
+          'css-loader',
+        ],
       },
       {
         test: /\.s[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+
+              
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
-    ]
-  }
+    ],
+  },
 };
